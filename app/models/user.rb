36cc85b7b2af3e6da577246d88310366baf3e928
Mouaz El Molkey <88675267+iammouaz@users.noreply.github.com>
2022-02-01 19:28:46 +0200

    Fix Linters

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,6 @@ class User < ApplicationRecord
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
   has_many :posts, dependent: :destroy
-  foreign_key: 'author_id'  
   def recent_posts(id)
     Post.order('created_at Desc').limit(3)  end
 end
