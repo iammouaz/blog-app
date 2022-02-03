3ba6ea6aed40178e0dc7f914a4947bd6fd7e68cd
iammouaz <88675267+iammouaz@users.noreply.github.com>
2022-02-04 01:05:42 +0200

    Fix Linters

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,19 +1,5 @@
 class ApplicationController < ActionController::Base
-<<<<<<< Updated upstream
-  def index
-    @users = User.all
-  end
-
-  def show
-    @user = User.find(params[:id])
-  end
-
   def current_user
     User.first
   end
-=======
-    def current_user
-        User.first
-      end
->>>>>>> Stashed changes
 end
