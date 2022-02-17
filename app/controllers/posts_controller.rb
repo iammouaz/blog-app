8f5c354244dd16c1a14976289a8b65c471661e4b
iammouaz <88675267+iammouaz@users.noreply.github.com>
2022-02-17 17:41:18 +0200

    Add Controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,5 @@
 class PostsController < ApplicationController
+  load_and_authorize_resource
   def index
     @user = User.find(params[:user_id])
     @posts = @user.posts.all.order(created_at: :desc)
@@ -11,18 +12,24 @@ class PostsController < ApplicationController
   end
 
   def new
+    @user = User.find(params[:user_id])
     @post = Post.new
   end
 
   def create
     @post = current_user.posts.new(post_params)
     if @post.save
-      @post.update_counter
+      @post.increase_counter
       flash[:notice] = 'Post was successfully created.'
       redirect_to [@post.user, @post]
     else
       flash[:alert] = 'Failed to create the post.'
     end
+  end
+
+  def destroy
+    @post.decrease_counter if @post.destroy
+    redirect_to user_posts_path
   end
 
   private
