9983af7370f693a0d99a388fc557fe25b452b3da
iammouaz <88675267+iammouaz@users.noreply.github.com>
2022-02-19 13:15:52 +0200

    Update  models and configs

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
-         :confirmable
+         :confirmable, :jwt_authenticatable,
+         jwt_revocation_strategy: JwtDenylist
   validates :name, :email, presence: true
   validates :posts_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
   has_many :comments, dependent: :destroy
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,11 @@ Rails.application.routes.draw do
   mount Rswag::Ui::Engine => '/api-docs'
   mount Rswag::Api::Engine => '/api-docs'
   devise_for :users, :controllers => {:registrations => "registrations"}
+  devise_for :users,
+  controllers: {
+      sessions: 'users/sessions',
+      registrations: 'users/registrations'
+  } 
   devise_scope :user do 
     get '/users/sign_out' => 'devise/sessions#destroy' 
   end
@@ -9,6 +14,11 @@ Rails.application.routes.draw do
     resources :posts, only: [:index, :show, :new, :create, :destroy] do
       resources :comments, only: [:new, :create, :destroy]
       resources :likes, only: [:create]
+    end
+  end
+  namespace :api, defaults: { format: :json } do
+    resources :posts, only: [:create, :index] do
+      resources :comments, only: [:create, :index]
     end
   end
   root 'users#index'
